b1e0a63d8a50d71aeb3606d5820edd3b9a569986
Antonio Martinez <insomnium098@gmail.com>
2018-08-02 16:42:52 -0500

    Se agregaron los complejos y sus colores

--- RegulonDBCytoscape/R/main.R
+++ RegulonDBCytoscape/R/main.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(RCy3)
+library(plyr)
 
 #x es la ruta de la carpeta con los archivos
 x <- c("AtoC")
@@ -97,7 +98,6 @@ cytoscapeRegulon <- function(x){
       for (j in seq_along(1:length(rownames(complejo_df)))){
         integrante_complejo <- complejo_df[j,]
         nombre_integrante_complejo <- as.character(integrante_complejo$type)
-        #Se añadira un espacio para que el nodo nuevo no tenga interaccion
         df_integrante_complejo <- data.frame("id" = paste0(
                                       nombre_integrante_complejo," "),
                                              "reactant_type" = "COMPLEX_NODE",
@@ -127,10 +127,62 @@ cytoscapeRegulon <- function(x){
   df_nodes <- rbind(df_nodes, df_nodes_complex)
 
 
+  ######Modificar el df de los complejos con los nuevos nombres con espacio
+
+  nombres_complejos <- as.character(complexes$type)
+  nombres_complejos <- paste0(nombres_complejos, ' ')
+
+  complexes$type <- nombres_complejos
+
+  ###Cambiar los colnames de complejes para posteriormente hacerlos edges
+  colnames(complexes) <- c("reaction_id_","target","source")
+
+  complexes$interaction <- rep("COMPLEX_INTERACTION",
+                               length(rownames(complexes)))
+
+  ##unir complexes con df_edges
+
+  df_edges <- rbind.fill(df_edges,complexes)
+
+
+
 
   #Llamar RCy3
   createNetworkFromDataFrames(df_nodes,df_edges, title=x,
                               collection=x)
+
+
+
+  ####Agrupar moleculas de los complejos
+
+  nodedata <- getTableColumns("node")
+  nodes_complexes <- nodedata[grep("COMPLEX_NODE", nodedata$reactant_type), ]
+
+
+  #####
+
+  # Seleccionar los complejos, uno por uno
+  for (g in unique(as.character(complexes$reaction_id_))){
+
+
+
+  deltacatnodes <- df_nodes[grep(g, df_nodes$name), "id"]
+  ##OBTENER EL SUID
+  SUIDS <- filter(nodedata,`shared name` == deltacatnodes)
+  SUIDS <- as.character(SUIDS$SUID)
+  ####
+  nodos_suids <- selectNodes(SUIDS, preserve=FALSE)
+  ###Seleccionar el primer nodo vecino
+  primer_vecino <- selectFirstNeighbors()
+  primer_vecino <- setdiff(primer_vecino,nodos_suids)
+  primer_vecino <- filter(nodedata, SUID == primer_vecino)
+  primer_vecino <- as.character(primer_vecino$`shared name`)
+
+  ##Crear grupo del complejo
+  createGroup(primer_vecino)
+  collapseGroup(primer_vecino)
+  }
+########
 
   #######Forma de los nodos
   #######
@@ -143,10 +195,12 @@ cytoscapeRegulon <- function(x){
   setNodeShapeMapping (column, values, shapes)
 
   #####Color de los nodos
+  ###FALTA EL DE LOS COMPLEX_NODES
   column <- 'reactant_type'
   values <- c ('SIMPLE_MOLECULE',  'PROTEIN',
-               'RNA',"GENE","COMPLEX","PROTEIN_TF")
-  colors <- c("#b6bd7b","#b6bd7b","#ffbc00","#ffbc00","#b6bd7b","#4881a6")
+               'RNA',"GENE","COMPLEX","PROTEIN_TF","COMPLEX_NODE")
+  colors <- c("#b6bd7b","#b6bd7b","#ffbc00","#ffbc00","#b6bd7b","#4881a6",
+              "#b6bd7b")
   setNodeColorMapping (column, values, colors,
                        mapping.type = "d")
 
@@ -155,7 +209,5 @@ cytoscapeRegulon <- function(x){
 
 
 
-
-
-
 }
+
